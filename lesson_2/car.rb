2407cf8cb08f39e134901ee1a845c595bfb20244
Ginni Pinckert <gcpinckert@gmail.com>
2021-08-20 09:27:10 -0700

    Adds examples and notes for testing

--- lesson_2/car.rb
+++ lesson_2/car.rb
@@ -4,4 +4,9 @@ class Car
   def initialize
     @wheels = 4
   end
+
+  # assert_equal for two Cars fails without this method
+  def ==(other)
+    other.is_a?(Car) && name == other.name
+  end
 end
